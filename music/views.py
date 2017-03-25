951a7e94abcee4b8169e153be1629de3297ddbce
konrad <konrad.schieban@gmail.com>
2017-03-24 21:42:38 -0400

    First Commit

--- music/views.py
+++ music/views.py
@@ -1,7 +1,33 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Album, Song
 
-# Create your views here.
 
 def index(request):
-    return HttpResponse('<h1>This is the Music Homepage</h1>')
+    all_albums = Album.objects.all()
+    context = {
+        'all_albums': all_albums,
+    }
+
+    return render(request, 'music/index.html', context)
+
+
+def detail(request, album_id):
+
+    album = get_object_or_404(Album, pk=album_id)
+    return render(request, 'music/detail.html', {'album': album})
+
+
+def favorite(request, album_id):
+    album = get_object_or_404(Album, pk=album_id)
+    try:
+        selected_song = album.song_set.get(pk=request.POST['song'])
+    except (KeyError, Song.DoesNotExist):
+        return render(request, 'music/detail.html', {
+            'album': album,
+            'error_message': "You did not select a valid song",
+        })
+    else:
+        selected_song.is_favorite = True
+        selected_song.save()
+        return render(request, 'music/detail.html', {'album': album})
+
